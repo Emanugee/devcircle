f3353fa2649422c3ee8657fcbf26a1bc4785818e
Emanugee <manugtech@gmail.com>
2022-08-30 12:16:15 +0100

    ip man

--- devcircle/settings.py
+++ devcircle/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-&41czj3n##!@#dq0zqm8s-3k)8$-eg$c094u2o7-(q31779-lb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['54.161.72.253']
+ALLOWED_HOSTS = ['54.234.214.28']
 
 
 # Application definition
